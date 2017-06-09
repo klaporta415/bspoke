a9ad3ae09bfa564ac41b671d62c028f2a616dc7a
Kaley La Porta & Brittany Baranowski <pair+klaporta415.minigiraffe23@devbootcamp.com>
2017-06-09 17:37:55 -0500

    Dotenv removed from application.rb, put Bundler.require back in

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
 
 GOOGLE_KEY = ENV['GOOGLE_KEY']
 module Bspoke

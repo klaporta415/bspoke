1165596e829744a1fc14bc3cbb9aaa2604f206ac
Daniel Wickman & Mumo Musau <pair+dwwickman.mmusau99@devbootcamp.com>
2017-06-09 21:52:06 -0500

    Pin creation upon marker placementgit add .git add .!

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,10 +1,16 @@
-class HomeController < ApplicationController
+class PinsController < ApplicationController
 
   def new
     @pin = Pin.new
   end
 
   def create
-
+    p params
+    @pin = Pin.create(pin_params)
   end
+
+  private
+    def pin_params
+      params.require(:pin).permit(:latitude, :longitude)
+    end
 end

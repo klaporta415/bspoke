1887368e1690c555fadfc7869b36fdefe4bd4de4
Daniel Wickman & Mumo Musau <pair+dwwickman.mmusau99@devbootcamp.com>
2017-06-12 15:51:06 -0500

    Pin model tests complete

--- spec/models/pins_spec.rb
+++ spec/models/pins_spec.rb
@@ -1,19 +1,55 @@
 require 'rails_helper'
 
 describe Pin do
-  let(:pin) { Pin.new(latitude: "41.876393", longitude: "-87.655474", category: "general") }
+  let!(:pin) { Pin.create(latitude: "41.876393", longitude: "-87.655474", category: "general") }
 
   describe "attributes" do
-   it "has a latitude" do
-    expect(pin.latitude).to eq "41.876393"
-   end
+    it "has a latitude" do
+      expect(pin.latitude).to eq "41.876393"
+    end
 
     it "has a longitude" do
       expect(pin.longitude).to eq "-87.655474"
-   end
-
+    end
     it "has a category" do
       expect(pin.category).to eq "general"
-   end
+    end
  end
+
+  describe "self.ordered_json" do
+    it "returns empty array when nothing selected" do
+      avoidance = "nothing"
+      expect(Pin.ordered_json(avoidance)).to eq '[]'
+    end
+
+    it "returns pin if everything selected" do
+      avoidance = "everything"
+      expect(Pin.ordered_json(avoidance)).to eq [pin].to_json
+    end
+
+    it "returns empty array when general not selected" do
+      avoidance = "goose"
+      expect(Pin.ordered_json(avoidance)).to eq '[]'
+    end
+
+    it "returns pin if general is selected" do
+      avoidance = "general"
+      expect(Pin.ordered_json(avoidance)).to eq [pin].to_json
+    end
+
+    it "returns empty array when smell selected" do
+      avoidance = "smell"
+      expect(Pin.ordered_json(avoidance)).to eq '[]'
+    end
+
+    it "returns empty array when event selected" do
+      avoidance = "event"
+      expect(Pin.ordered_json(avoidance)).to eq '[]'
+    end
+
+    it "returns empty array when road selected" do
+      avoidance = "roadCondition"
+      expect(Pin.ordered_json(avoidance)).to eq '[]'
+    end
+  end
 end

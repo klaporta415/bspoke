22572206f24671a1e85a7e24b082824e267bfd29
Daniel Wickman <email": null>
2017-06-15 15:01:43 -0500

    comment out method for reroute

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  # private
+
+  # def affter_sign_in_path_for(resource)
+  #   session["user_return_to"] || home_map_path
+  # end
+
 end
